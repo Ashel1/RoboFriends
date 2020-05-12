e0a6c4f05ea6b28a9f07527390cd983ae27f8fff
Ashutosh <ashukakoria@gmail.com>
2020-05-12 14:34:41 +0530

    Updates

---
+++ precache-manifest.581ea3e9397e588be5ef750a5201712a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fa5f835d9b34faaa3189fe1445d892",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "626000f716e3316fa86d",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "a7adac037bb8dfa877c3",
+    "url": "/robofriends/static/css/main.6727affb.chunk.css"
+  },
+  {
+    "revision": "626000f716e3316fa86d",
+    "url": "/robofriends/static/js/2.d26dd189.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.d26dd189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7adac037bb8dfa877c3",
+    "url": "/robofriends/static/js/main.5845a6e7.chunk.js"
+  },
+  {
+    "revision": "3a6cd7bf0ee3b3b38bcf",
+    "url": "/robofriends/static/js/runtime-main.db3a75f1.js"
+  }
+]);
